2b4a2702d54a39b2d095e2075db25c645f905bb8
emran0335 <emranhossain0335@gmail.com>
2025-01-05 02:06:17 +0600

    all is okay

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,35 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+if (!MONGODB_URL) {
+  throw new Error(
+    "Please define the MONGODB_URL environment variable in .env.local"
+  );
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+let cached = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
-
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "imaginify",
-      bufferCommands: false,
-    });
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL).then((mongoose) => mongoose);
+  }
 
   cached.conn = await cached.promise;
-
   return cached.conn;
-};
+}
